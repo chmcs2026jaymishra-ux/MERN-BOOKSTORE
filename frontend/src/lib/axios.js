ec7bf9479b5c93af0a8fe3a0a54f3c62a4689f54
chmcs2026jaymishra-ux <chmcs2026.jay.mishra@gmail.com>
2026-02-21 12:33:53 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({
 //     baseURL: 'http://localhost:3000'
 // })
-        baseURL: '    baseURL: 'http://localhost:3000'
+        baseURL: '  https://mern-bookstore-bdq9.onrender.com
 })'
 })
     
