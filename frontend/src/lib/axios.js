aee54887fd93efd28756c272f5dc33c4eb5d7add
chmcs2026jaymishra-ux <chmcs2026.jay.mishra@gmail.com>
2026-02-21 12:32:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+//     baseURL: 'http://localhost:3000'
+// })
+        baseURL: '    baseURL: 'http://localhost:3000'
+})'
 })
+    
 export default api
